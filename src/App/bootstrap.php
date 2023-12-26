d638cda842529d66e12a3d817dd3df4e115f9797
Boris Puhačin <113792858+puhacinboris@users.noreply.github.com>
2023-12-26 15:49:42 +0100

    Created Home Controller and updated Router

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers;
+use App\Controllers\HomeController;
+
 
 $app = new App();
 
-$app->get('/');
-
-
-dd($app);
+$app->get('/', [HomeController::class, 'home']);
 
 return $app;
